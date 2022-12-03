62d1a60930280b6a6ecfb920227ba48feda87427
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 18:37:01 -0500

    created method to get read me

---
+++ web/src/hooks/useGetReadMe.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Repo } from '../models/Repo';
+import useReposStore from '../store/repoStore';
+
+type props = {
+  repoName: string | undefined;
+};
+
+export default function useGetReadMe({ repoName }: props) {
+  const [readMe, setReadMe] = useState('');
+  const [error, setError] = useState<Error>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (repoName) {
+      fetch(`https://raw.githubusercontent.com/${repoName}/master/README.md`, {
+        method: 'GET',
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw response;
+        })
+        .then((data: string) => {
+          setReadMe(data);
+        })
+        .catch((err) => {
+          console.error('Error while fetching for data: ', err);
+          setError(err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, [repoName]);
+
+  return { readMe, error, isLoading };
+}
