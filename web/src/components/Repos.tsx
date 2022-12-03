e53313e549676da9885775586e8cb9773a12d3a9
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 17:05:04 -0500

    renamed and added filter by languages

--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
 import { Repo } from '../models/Repo';
+import FilterByLanguages from './FilterByLanguages';
+import IndividualRepo from './IndividalRepo';
 
 type props = { repos: Repo[] };
 
 export default function Repos({ repos }: props) {
   const [selectedLanguage, setSelectedLanguage] = useState('all');
   const languages = getLanguages({ repos });
-  return <></>;
+  return (
+    <>
+      <FilterByLanguages
+        languages={languages}
+        setSelectedLanguage={setSelectedLanguage}
+      ></FilterByLanguages>
+
+      {repos.length > 0 ? (
+        repos.map((repo) => <IndividualRepo repo={repo}></IndividualRepo>)
+      ) : (
+        <p>No repositories found.</p>
+      )}
+    </>
+  );
 }
 
 function getLanguages({ repos }: { repos: Repo[] }): string[] {
