43c3985e2c21a8baff9197b223f3ef0cdf65ec76
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 18:11:49 -0500

    fixed user login

--- web/src/pages/repo/repo.tsx
+++ web/src/pages/repo/repo.tsx
@@ -4,15 +4,16 @@ import useReposStore from '../../store/repoStore';
 
 export default function Repo() {
   const { repoId } = useParams();
+  console.log(repoId);
   const repos = useReposStore((state) => state.repos);
   const repo = getRepo({ id: parseInt(repoId ?? '0') });
   return (
     <div>
       <Link to="/">Back to Home</Link>
-      <p>Commit Date: {repo?.updated_at}</p>
-      <p>Author: {repo?.owner}</p>
-      <p>Message: {repo?.commits_url}</p>
-      // TODO fetch read me
+      <p>Commit Date: {repo?.updated_at ?? 'Not available'}</p>
+      <p>Author: {repo?.owner.login ?? 'Not available'}</p>
+      <p>Message: {repo?.commits_url ?? 'Not available'}</p>
+      {/*  TODO fetch read me */}
     </div>
   );
 
