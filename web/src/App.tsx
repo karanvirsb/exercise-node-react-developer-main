eded12cb7e7605eab3ebd75da9416f3c3983fe1a
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 16:22:23 -0500

    added router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
 
 export function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
