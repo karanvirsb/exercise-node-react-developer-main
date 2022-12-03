7253bc2f2b536dcceaf79daf0bd554e940280596
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 15:52:14 -0500

    created home page

---
+++ web/src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Repositories</h1>
+      {/* TODO add language filter */}
+      <div></div>
+    </div>
+  );
+}
