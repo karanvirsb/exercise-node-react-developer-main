d42b7a6e291a5ba8f3107af653707a4967cfdf29
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 16:02:17 -0500

    added error message and returned data

--- web/src/hooks/useGetRepos.ts
+++ web/src/hooks/useGetRepos.ts
@@ -4,12 +4,18 @@ type props = { url: string };
 
 export default function useGetRepos({ url }: props) {
   const [repos, setRepos] = useState([]);
+  const [errMsg, setErrMsg] = useState<string>('');
   useEffect(() => {
     async function fetchRepos() {
-      const data = fetch(url, { method: 'GET' });
-      console.log(data);
+      try {
+        const data = await fetch(url, { method: 'GET' });
+        if (data) setRepos(await data.json());
+        setRepos([]);
+      } catch (error) {}
     }
 
     fetchRepos();
   }, [url]);
+
+  return { repos, errMsg };
 }
