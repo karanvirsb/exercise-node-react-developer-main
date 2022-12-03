32c6049b0a7bd4803c034768aa4418947cb6f73a
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 16:30:29 -0500

    added is loading, error handling and displaying

--- web/src/pages/home/home.tsx
+++ web/src/pages/home/home.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Repos from '../../components/Repos';
+import useGetRepos from '../../hooks/useGetRepos';
 
 export default function Home() {
+  const { repos, error, isLoading } = useGetRepos();
+  if (isLoading) {
+    return <p>...Loading</p>;
+  }
+
+  if (error) {
+    return <p>An error occurred while fetching. Please refresh.</p>;
+  }
+
   return (
     <div>
       <h1>Repositories</h1>
       {/* TODO add language filter */}
-      <div></div>
+      <div>
+        {repos.length > 0 ? (
+          repos.map((repo) => <Repos repo={repo}></Repos>)
+        ) : (
+          <p>No repositories found.</p>
+        )}
+      </div>
     </div>
   );
 }
