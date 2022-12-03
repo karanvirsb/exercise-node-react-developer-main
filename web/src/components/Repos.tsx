a7876109488a7c6bffa7a82532dfbe55794dd471
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 17:17:12 -0500

    added props selectedLanguage to filters

--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -17,6 +17,7 @@ export default function Repos({ repos }: props) {
     <>
       <FilterByLanguages
         languages={languages}
+        selectedLanguage={selectedLanguage}
         setSelectedLanguage={setSelectedLanguage}
       ></FilterByLanguages>
 
