567cdc18f07e7a931873c368254ace1a3d19a265
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 16:21:30 -0500

    updated and used then and catch

--- web/src/hooks/useGetRepos.ts
+++ web/src/hooks/useGetRepos.ts
@@ -1,21 +1,31 @@
 import { useEffect, useState } from 'react';
+import { Repo } from '../models/Repo';
 
-type props = { url: string };
-
-export default function useGetRepos({ url }: props) {
-  const [repos, setRepos] = useState([]);
-  const [errMsg, setErrMsg] = useState<string>('');
+export default function useGetRepos() {
+  const [repos, setRepos] = useState<Repo[]>([]);
+  const [error, setError] = useState<Error>();
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    async function fetchRepos() {
-      try {
-        const data = await fetch(url, { method: 'GET' });
-        if (data) setRepos(await data.json());
-        setRepos([]);
-      } catch (error) {}
-    }
+    fetch('http:localhost:4000/repos', {
+      method: 'GET',
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data: Repo[]) => {
+        setRepos(data);
+      })
+      .catch((err) => {
+        console.error('Error while fetching for data: ', err);
+        setError(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-    fetchRepos();
-  }, [url]);
-
-  return { repos, errMsg };
+  return { repos, error, isLoading };
 }
