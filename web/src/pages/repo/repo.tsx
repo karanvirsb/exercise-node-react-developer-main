725cadaf5b08b7a3bfe6dc14c27ed24d1cf8547f
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 18:49:41 -0500

    added loading and error state along with read me

--- web/src/pages/repo/repo.tsx
+++ web/src/pages/repo/repo.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
+import useGetReadMe from '../../hooks/useGetReadMe';
 import useReposStore from '../../store/repoStore';
 
 export default function Repo() {
   const { repoId } = useParams();
   const repos = useReposStore((state) => state.repos);
   const repo = getRepo({ id: parseInt(repoId ?? '0') });
+  const { readMe, error, isLoading } = useGetReadMe({
+    repoName: repo?.full_name,
+  });
   return (
     <div>
       <Link to="/">Back to Home</Link>
       <p>Commit Date: {repo?.updated_at ?? 'Not available'}</p>
       <p>Author: {repo?.owner.login ?? 'Not available'}</p>
       <p>Message: {repo?.commits_url ?? 'Not available'}</p>
-      {/*  TODO fetch read me */}
+      <div>
+        <h1>Read Me</h1>
+        <p>
+          {isLoading
+            ? '...loading'
+            : error
+            ? 'Could fetch read me.'
+            : readMe ?? 'Not available'}
+        </p>
+      </div>
     </div>
   );
 
