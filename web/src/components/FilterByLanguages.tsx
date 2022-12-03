ca9785b9aa743d99ba5671fd534dbf42e3aacebd
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 17:16:51 -0500

    added css to active filter

--- web/src/components/FilterByLanguages.tsx
+++ web/src/components/FilterByLanguages.tsx
@@ -2,21 +2,31 @@ import React from 'react';
 
 type props = {
   languages: string[];
+  selectedLanguage: string;
   setSelectedLanguage: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export default function FilterByLanguages({
   setSelectedLanguage,
+  selectedLanguage,
   languages,
 }: props) {
   return (
     <div>
       {languages.map((lang) => {
-        return (
-          <button key={lang} onClick={() => handleClick(lang)}>
-            {lang}
-          </button>
-        );
+        if (lang === selectedLanguage) {
+          return (
+            <button key={lang} className="active_filter">
+              {lang}
+            </button>
+          );
+        } else {
+          return (
+            <button key={lang} onClick={() => handleClick(lang)}>
+              {lang}
+            </button>
+          );
+        }
       })}
     </div>
   );
