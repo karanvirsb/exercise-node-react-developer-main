478c4ad64f54823e88a91ed478ab689366457e7e
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 15:26:45 -0500

    created a method to get repo data

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from 'express';
+import axios, { AxiosError } from 'axios';
+import { Repo } from '../models/Repo';
 
 export const repos = Router();
 
@@ -8,5 +10,19 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
+
   res.json([]);
 });
+
+async function getRepoData(): Promise<Repo[]> {
+  const url = 'https://api.github.com/users/silverorange/repos';
+  try {
+    const resp = await axios({ url, method: 'GET' });
+    return resp.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw Error(error.message);
+    }
+  }
+  return [];
+}
