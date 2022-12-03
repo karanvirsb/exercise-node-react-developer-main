cd08fdfb297fd1a86971f1fcb8e42a264cdb4cb1
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 17:58:52 -0500

    added route to repo page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
+import Repo from './pages/repo/repo';
 
 export function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/repo/:repoId" element={<Repo />} />
       </Routes>
     </Router>
   );
