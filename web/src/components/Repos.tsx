33ab63ed458e01abd1fc675ade9fbca0ec5f65f7
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 16:45:42 -0500

    added languages

--- web/src/components/Repos.tsx
+++ web/src/components/Repos.tsx
@@ -4,11 +4,13 @@ import { Repo } from '../models/Repo';
 type props = { repos: Repo[] };
 
 export default function Repos({ repos }: props) {
+  const languages = getLanguages({ repos });
   return <div>Repos</div>;
 }
 
 function getLanguages({ repos }: { repos: Repo[] }): Set<string> {
   const languages: string[] = repos.map((repo) => repo.language);
   const langSet = new Set(languages);
+  langSet.add('all');
   return langSet;
 }
