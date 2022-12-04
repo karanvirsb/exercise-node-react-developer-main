4a5568f4e540feb2b0fca83044510b30ad5c061e
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 19:00:04 -0500

    created function to sort repos in reverse chronological order

--- web/src/util/filterRepos.ts
+++ web/src/util/filterRepos.ts
@@ -6,7 +6,18 @@ type props = { repos: Repo[]; language: string };
 // TODO do it in chronological order
 export default function filterRepos({ repos, language }: props) {
   if (language.toLowerCase() === 'all') {
-    return repos;
+    return sortRepos({ repos });
   }
-  return repos.filter((repo) => repo.language !== language);
+  return sortRepos({
+    repos: repos.filter((repo) => repo.language !== language),
+  });
+}
+
+function sortRepos({ repos }: { repos: Repo[] }) {
+  return repos
+    .sort(
+      (a, b) =>
+        new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+    )
+    .reverse();
 }
