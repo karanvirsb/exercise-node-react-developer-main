9952694f814516c82c755d2ed8e87056f5d4ae3e
mrboparai <karanvirboparai123@gmail.com>
2022-12-03 19:13:07 -0500

    created fetch for commits

---
+++ web/src/hooks/useGetCommits.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import { Commit } from '../models/Commit';
+
+type props = {
+  repoName: string | undefined;
+};
+
+export default function useGetCommits({ repoName }: props) {
+  const [commits, setCommits] = useState<Commit[]>([]);
+  const [error, setError] = useState<Error>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (repoName) {
+      //api.github.com/repos/silverorange/Castanet/commits
+      https: fetch(`api.github.com/${repoName}/commits`, {
+        method: 'GET',
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw response;
+        })
+        .then((data: Commit[]) => {
+          setCommits(data);
+        })
+        .catch((err) => {
+          console.error('Error while fetching for data: ', err);
+          setError(err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, [repoName]);
+
+  return { commits, error, isLoading };
+}
